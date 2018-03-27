01c3d35192b29fed39222019730d55b12d5cd5b4
Carlos H <carlos.e.herrera.j@hotmail.com>
2018-03-27 01:31:50 -0400

    Sync 27/03/2018 - 01:27 AM
    Sync 27/03/2018 - 01:27 AM

---
+++ Lands/lands/Lands.BackEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lands.BackEnd.MvcApplication" Language="C#" %>
---
+++ Lands/lands/Lands.BackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lands.BackEnd.Startup))]
+namespace Lands.BackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
